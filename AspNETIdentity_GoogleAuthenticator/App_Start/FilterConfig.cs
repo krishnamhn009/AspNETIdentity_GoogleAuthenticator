4f205b9756adcaa73273bdb7c9a83c7ef5faa9a2
Krishna yadav <krishnamhn009@gmail.com>
2018-02-13 09:15:47 +0530

    Add project files.

---
+++ AspNETIdentity_GoogleAuthenticator/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AspNETIdentity_GoogleAuthenticator
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
