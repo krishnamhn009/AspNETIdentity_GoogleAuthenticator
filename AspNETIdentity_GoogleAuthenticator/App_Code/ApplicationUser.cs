f002064a03b2d7dd34c6fba624590a24dbabac1e
Krishna yadav <krishnamhn009@gmail.com>
2018-02-13 09:29:18 +0530

    Added Google Authenticator project

---
+++ AspNETIdentity_GoogleAuthenticator/App_Code/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNETIdentity_GoogleAuthenticator.App_Code
+{
+    public class ApplicationUser
+    {
+        public bool IsGoogleAuthenticatorEnabled { get; set; }
+
+        public string GoogleAuthenticatorSecretKey { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+}
