b1792244959ec3c083601f9572e14b5d8183828d
Krishna yadav <krishnamhn009@gmail.com>
2018-02-15 10:12:47 +0530

    added AuthenticatorTokenProvider class

--- AspNETIdentity_GoogleAuthenticator/App_Code/ApplicationUser.cs
+++ AspNETIdentity_GoogleAuthenticator/App_Code/ApplicationUser.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace AspNETIdentity_GoogleAuthenticator.App_Code
 {
-    public class ApplicationUser
+    public class ApplicationUser : IdentityUser
     {
         public bool IsGoogleAuthenticatorEnabled { get; set; }
 
---
+++ AspNETIdentity_GoogleAuthenticator/App_Code/GoogleAuthenticatorTokenProvider.cs
@@ -0,0 +1,40 @@
+﻿using Base32;
+using Microsoft.AspNet.Identity;
+using OtpSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AspNETIdentity_GoogleAuthenticator.App_Code
+{
+    public class GoogleAuthenticatorTokenProvider:IUserTokenProvider<ApplicationUser,string>
+    {
+        public Task<string> GenerateAsync(string purpose, UserManager<ApplicationUser, string> manager, ApplicationUser user)
+        {
+            return Task.FromResult((string)null);
+        }
+
+        public Task<bool> ValidateAsync(string purpose, string token, UserManager<ApplicationUser, string> manager, ApplicationUser user)
+        {
+            long timeStepMatched = 0;
+
+            var otp = new Totp(Base32Encoder.Decode(user.GoogleAuthenticatorSecretKey));
+            bool valid = otp.VerifyTotp(token, out timeStepMatched, new VerificationWindow(2, 2));
+
+            return Task.FromResult(valid);
+        }
+
+        public Task NotifyAsync(string token, UserManager<ApplicationUser, string> manager, ApplicationUser user)
+        {
+            return Task.FromResult(true);
+        }
+
+        public Task<bool> IsValidProviderForUserAsync(UserManager<ApplicationUser, string> manager, ApplicationUser user)
+        {
+            return Task.FromResult(user.IsGoogleAuthenticatorEnabled);
+        }
+
+    }
+}
